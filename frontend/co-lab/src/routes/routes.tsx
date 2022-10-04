0c59f37f4aa9a01a24d2f51607fe65131d78191c
Refilwe-M-Mashile <=>
2022-10-04 15:58:35 +0200

    Initial Router setup

--- frontend/co-lab/src/routes/routes.tsx
+++ frontend/co-lab/src/routes/routes.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Heading } from "../components/heading/heading";
+import { Index } from "../pages/index";
 
 export function StaticRoute() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Heading />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
