04857a868b1b31d056ca4f32375501ea2d5f6489
Refilwe-M-Mashile <=>
2022-10-04 16:49:34 +0200

    Done: POC routes

--- frontend/co-lab/src/routes/routes.tsx
+++ frontend/co-lab/src/routes/routes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Index } from "../pages/index";
+import { SetupForm, Index, LinkInfo } from "../pages";
 
 export function StaticRoute() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/setup" element={<SetupForm />} />
+        <Route path="/session-info" element={<LinkInfo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
