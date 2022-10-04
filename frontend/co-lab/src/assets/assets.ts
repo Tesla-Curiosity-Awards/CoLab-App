fe8678874b529133cc21c8ad4a1f5049ddc7fae3
Refilwe-M-Mashile <=>
2022-10-04 15:58:19 +0200

    Assets setup

---
+++ frontend/co-lab/src/assets/assets.ts
@@ -0,0 +1,6 @@
+import completeIcon from "./icons/compete.png";
+import teamWorkIcon  from "./icons/team-work.png";
+import partnershipIcon from "./icons/partnership.png"
+import homeSVG from "./images/collab.svg"
+
+export {completeIcon,teamWorkIcon,partnershipIcon, homeSVG}
