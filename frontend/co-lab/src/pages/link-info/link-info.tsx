f0f98ca46d440502a10672d54f260e99cf45facf
Refilwe-M-Mashile <=>
2022-10-07 13:54:29 +0200

    WIP: Link Page

--- frontend/co-lab/src/pages/link-info/link-info.tsx
+++ frontend/co-lab/src/pages/link-info/link-info.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { qrCode } from '../../assets/assets'
+import { Footer } from '../../components/footer/footer';
+import './link-info.scss'
 
 export const LinkInfo = () => {
+    const [copyStatus, setCopied] = useState('Copy Link');
     return (
-        <div>LinkInfo</div>
+        <>
+            <section className='session-info'>
+                <section className='qr'>
+                    <img src={qrCode} alt="qr code" />
+                </section>
+                <section className={`session-link`}>
+                    <p className='share-text'>Share link<span className={`btn-copy ${copyStatus === 'Copied' ? 'copied' : ''}`} onClick={() => {
+                        navigator.clipboard.writeText(window.location.protocol + '//' + window.location.host +'/tegd6-dggd-dgag-dtefvf')
+                        setCopied('Copied')
+                    }}>{copyStatus}</span></p>
+                </section>
+            </section>
+            <Footer />
+        </>
     )
 }
