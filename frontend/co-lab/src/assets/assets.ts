8e9ba0fad6b69f53848f5b6da3d719083cca5bc5
Refilwe-M-Mashile <=>
2022-10-07 13:54:49 +0200

    WIP

--- frontend/co-lab/src/assets/assets.ts
+++ frontend/co-lab/src/assets/assets.ts
@@ -2,5 +2,6 @@ import completeIcon from "./icons/compete.png";
 import teamWorkIcon  from "./icons/team-work.png";
 import partnershipIcon from "./icons/partnership.png"
 import homeSVG from "./images/collab.svg"
+import qrCode from "./icons/qr-code.png"
 
-export {completeIcon,teamWorkIcon,partnershipIcon, homeSVG}
+export {completeIcon,teamWorkIcon,partnershipIcon, homeSVG, qrCode}
--- frontend/co-lab/src/pages/setup-form/setup-form.tsx
+++ frontend/co-lab/src/pages/setup-form/setup-form.tsx
@@ -1,55 +1,64 @@
+
+import { Formik, Field, FieldProps } from "formik";
 import { Footer } from "../../components/footer/footer";
 
 import './setup-form.scss'
 
+interface FormProps {
+  username: string,
+
+}
 export const SetupForm = () => {
+  const initialValues: FormProps = { username: "" }
+
   return (
     <>
       <section id="card">
         <section id="title">Create Session</section>
-        <form
-          onSubmit={() => {
-            window.location.href = '/session-info'
-          }}
-        >
-          <section className="user-sect">
-            <label className="section-heading" htmlFor="username">
-              Username:
-            </label>
+        <Formik initialValues={initialValues} onSubmit={() => { }}>
+          <form
+            
+            onSubmit={(e) => { e.preventDefault()}}
+          >
+            <section className="user-sect">
+              <label className="section-heading" htmlFor="username">
+                Username:
+              </label>
 
-            <section className="row">
-              <input type="text" name="username" id="username" />
-              <select title="avatar" name="avatar" id="avatar">
-                <option value="">Style</option>
-                <option value="kinetic">🎨</option>
-                <option value="default">✍🏻</option>
-                <option value="visual">👀</option>
-              </select>
+              <section className="row">
+                <input type="text" name="username" id="username" />
+                <select title="avatar" name="avatar" id="avatar">
+                  <option value="">Style</option>
+                  <option value="kinetic">🎨</option>
+                  <option value="default">✍🏻</option>
+                  <option value="visual">👀</option>
+                </select>
+              </section>
             </section>
-          </section>
-          <section id="role-sect">
-            <p className="section-heading">Role</p>
-            <section className="radio-group">
-              <p>
-                <input title="lead" type="radio" value="Lead" name="lead" /> Lead
-              </p>
-              <p>
-                <input title="dev" type="radio" value="Dev" name="dev" /> Developer
-              </p>
-              <p>
-                <input title="watcher" type="radio" value="Watcher" name="watcher" /> Watcher
-              </p>
+            <section id="role-sect">
+              <p className="section-heading">Role</p>
+              <section className="radio-group">
+                <p>
+                  <input title="lead" type="radio" value="Lead" name="lead" /> Lead
+                </p>
+                <p>
+                  <input title="dev" type="radio" value="Dev" name="dev" /> Developer
+                </p>
+                <p>
+                  <input title="watcher" type="radio" value="Watcher" name="watcher" /> Watcher
+                </p>
+              </section>
             </section>
-          </section>
-          <section className="bottom">
-            <button type="submit" className="secondary">
-              Cancel
-            </button>
-            <button type="submit" className="primary">
-              Create
-            </button>
-          </section>
-        </form>
+            <section className="bottom">
+              <button type="reset" className="secondary" onClick={() => window.location.href = '/'}>
+                Cancel
+              </button>
+              <button type="submit" className="primary" onClick={() => window.location.href = '/session-info'}>
+                Create
+              </button>
+            </section>
+          </form>
+        </Formik>
       </section>
       <Footer />
     </>
