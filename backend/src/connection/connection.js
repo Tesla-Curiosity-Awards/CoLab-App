c446e5f430b6b560c606f23bbcd0bacb61b2978b
Refilwe-M-Mashile <=>
2022-10-28 14:16:23 +0200

    Done: Connects to DB

---
+++ backend/src/connection/connection.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+
+const connect = async () => {
+  const sequelize = new Sequelize("postgres://postgres@localhost:5432/colab"); // Example for postgres
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+connect();
