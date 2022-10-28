776cf9a62c613baad2c66a7d31ebbe169f3b6344
Refilwe-M-Mashile <=>
2022-10-28 11:10:28 +0200

    WIP:Nav Bar

--- frontend/co-lab/src/components/top-naviagtion-bar/top-nav-bar.tsx
+++ frontend/co-lab/src/components/top-naviagtion-bar/top-nav-bar.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { logo } from '../../assets/images/index'
+import './top-nav-bar.scss'
 
-const NavBarTop = () => {
+const TopNavBar = () => {
   return (
     <nav>
-        
+      <div className="left">
+        <img className="logo" src={logo} alt="Logo" />
+      </div>
+      <div className="right">
+        <ul className='nav-list'>
+          <li className='nav-item'><a href='/setup'>Create Session</a></li>
+          <li className='nav-item'><a href='#'>About Us</a></li>
+        </ul>
+      </div>
     </nav>
   )
 }
 
-export default NavBarTop
+export default TopNavBar
