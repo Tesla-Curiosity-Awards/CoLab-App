30bfe5ffa6213d2f21f34fc0e02d520a716fa33b
Refilwe-M-Mashile <=>
2022-11-04 22:29:22 +0200

    basic CRU done for Avatars

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,9 +3,39 @@ const router = express.Router();
 const db = require("../config/db.config");
 const { Avatar } = require("../models/Colab");
 
-router.get("/", async (req, res) => {
-  const result = await Avatar.findAll();
-  console.log(result);
+router.get("/avatars", async (req, res, err) => {
+  const result = err
+    ? await Avatar.findAll()
+    : "Oops something went wrong, Please try again later";
+  res.send(result);
+});
+
+router.get("/avatars/:id", async (req, res, err) => {
+  const {id} = req.params;
+  const result = err
+    ? await Avatar.findAll({
+      where:{
+        id : id
+      }
+    })
+    : "Oops something went wrong, Please try again later";
+  res.send(result);
+});
+
+router.post("/avatars", async (req, res) => {
+  const {avatar} = req.body;
+  const result = await Avatar.create({avatar});
+  console.log('added avatar');
+  res.sendStatus(200);
+});
+
+router.put("/avatars/:id", async (req, res) => {
+  const {id} = req.params;
+  const {avatar} = req.body;
+  const result = await Avatar.update({avatar},{where:{
+    id: id
+  }});
+  console.log('updated avatar');
   res.sendStatus(200);
 });
 
